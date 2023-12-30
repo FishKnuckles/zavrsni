aee9a7b21e40510d44daff8c858747326542aa7b
FishKnuckles <101805019+FishKnuckles@users.noreply.github.com>
2023-12-30 18:13:53 +0100

    Add files via upload

--- play.js
+++ play.js
@@ -1,6 +1,19 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
+
+let grid = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 0, 8, 9],
+    [1, 2, 0, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 0, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  ];
+
 function Draw() {
     parent_bb = canvas.parentElement.getBoundingClientRect();
     canvas.width = parent_bb.width;
@@ -8,6 +21,7 @@ function Draw() {
 
     let min = Math.min(canvas.width, canvas.height);
     let gridSize = min*0.8;
+    let cellSize = gridSize/9 ;
     let xOffset = (canvas.width - gridSize)/2;
     let yOffset = (canvas.height - gridSize)/2;
 
@@ -22,12 +36,28 @@ function Draw() {
         else {
             ctx.lineWidth = 1;
         }
-        ctx.moveTo(xOffset+gridSize/9*i, yOffset);
-        ctx.lineTo(xOffset+gridSize/9*i, yOffset+gridSize);
-        ctx.moveTo(xOffset, yOffset+gridSize/9*i);
-        ctx.lineTo(xOffset+gridSize, yOffset+gridSize/9*i);
+        ctx.moveTo(xOffset+cellSize*i, yOffset);
+        ctx.lineTo(xOffset+cellSize*i, yOffset+gridSize);
+        ctx.moveTo(xOffset, yOffset+cellSize*i);
+        ctx.lineTo(xOffset+gridSize, yOffset+cellSize*i);
         ctx.stroke();
     }
+    let fontSize = cellSize/2;
+    ctx.font = `${fontSize}px Arial`;
+    let x, y, xCenter, yCenter, textSize;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 0) continue;
+            xCenter = (cellSize - ctx.measureText(grid[i][j]).width)/2;
+            yCenter = (7/8*cellSize - fontSize)/2 + fontSize;
+            x = xOffset+xCenter+cellSize*j;
+            y = yOffset+yCenter+cellSize*i;
+            console.log(x, y);
+            ctx.fillText(grid[i][j], x, y);
+        }
+
+    }
+    
 }
 Draw();
 window.addEventListener("resize", Draw);
